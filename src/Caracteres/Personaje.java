b1429b3b16f0775d57628394266b9c97c636ca56
Juan Jose <juan2007acosta@gmail.com>
2026-05-26 15:19:00 -0500

    Clase Personaje

---
+++ src/Caracteres/Personaje.java
@@ -0,0 +1,44 @@
+package Caracteres;
+
+public class Personaje {
+    private String nombre;
+    private int vida;
+    protected int damage;
+    //Constructor
+    public Personaje(String nombre, int vida, int damage){
+        this.nombre=nombre;
+        this.setVida(vida);
+        this.setDamage(damage);
+    }
+    //Getters
+    public String getName() {
+        return nombre;
+    }
+    public int getDamage(){
+        return damage;
+    }
+    public int getVida() {
+        return vida;
+    }
+
+    //Setters
+    public void setVida(int vida) {
+        if (vida < 0){
+            vida = 0; // la vida nunca puede ser negativa
+        }else{
+            this.vida = vida;
+
+        }
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public String atacar() {
+        return nombre + " realiza un ataque basico.";
+    }
+    @Override
+    public String toString(){
+        return nombre + " | Vida: " + vida + " | Damage: " + damage;
+    }
+
+}
